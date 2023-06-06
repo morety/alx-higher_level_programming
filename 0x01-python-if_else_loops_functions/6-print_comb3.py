9b4545c886ce7d333ac4e9aaf11f1d7a9190fc77
morety <muretisammy@gmail.com>
2023-06-06 11:55:57 -0700

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+last_number = 89
+
 for letters in range(10):
-    for num in range(letters+1, 10):
-        print("{:02d},".format(letters*10+num), end=" ")
+    for num in range(letters + 1, 10):
+        current_number = letters * 10 + num
+        if current_number == last_number:
+            print("{:02d}".format(current_number), end=" ")
+        else:
+            print("{:02d},".format(current_number), end=" ")
 print()
